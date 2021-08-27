874dd359624a6cf9d7b97b5ed6f0d884428facf8
NotMyMajor <50533984+NotMyMajor@users.noreply.github.com>
2021-08-27 12:44:50 -0500

    Adding licensing info

--- BMP2PDF.py
+++ BMP2PDF.py
@@ -1,3 +1,8 @@
+### NotMyMajor/Python-Bitmaps-to-PDF is licensed under the
+### GNU General Public License v3.0
+### Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+### See COPYING.txt for full licensing information.
+
 ### Welcome to bmp2pdf! A quick little script to convert a folder of .bmp files into a single PDF.
 ### This script can be used as a function or run by itself. 
 ### If you choose to run the file by itself, check the options at the bottom. 
